7364922faba503bb7ed343e7b426e0288caca480
Priyanshualive <priyanshuk187@gmail.com>
2024-10-18 23:11:36 +0530

    Awesome

---
+++ script.js
@@ -0,0 +1,13 @@
+// var tablinks = document.getElementsByClassName("tab-links");
+// var tabcontents = document.getElementsByClassName("tab-contents");
+
+// function opentab(tabname){
+//     for (tl of tablinks) {
+//         tl.classList.remove("active-link");
+//     }
+//     for (tc of tabcontents) {
+//         tc.classList.remove("active-tab");
+//     }
+//     event.currentTarget.classList.add("active-link");
+//     document.getElementsById(tabname).classList.add("active-tab");
+// }
